27db5e642d2c20b4cd59275f451c3d6e99b4f852
samu2421 <95489339+samu2421@users.noreply.github.com>
2022-03-04 18:17:11 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,14 @@
+#Fibonacci numbers
+
+import math
+ 
+def fibo(n):
+    phi = (1 + math.sqrt(5)) / 2
+    return round(pow(phi, n) / math.sqrt(5))
+       
+if __name__ == '__main__':
+     
+    n = int(input("Enter the index "))
+     
+    print(fibo(n))
+ 
